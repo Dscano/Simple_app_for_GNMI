fd8a811367b07d5f24de1f28d0b409c7c8e67003
Dscano <d.scano89@gmail.com>
2020-01-23 18:08:36 +0100

    test subscription

--- src/main/java/org/gnmitest/app/Testgnmi.java
+++ src/main/java/org/gnmitest/app/Testgnmi.java
@@ -16,14 +16,21 @@
 package org.gnmitest.app;
 
 import com.google.common.util.concurrent.Futures;
+import org.onosproject.net.Port;
+import org.onosproject.net.PortNumber;
+import org.onosproject.net.device.DeviceService;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ReferenceCardinality;
+import gnmi.Gnmi.SubscriptionList;
+import gnmi.Gnmi.SubscriptionMode;
+import gnmi.Gnmi.Subscription;
 import gnmi.Gnmi;
 import gnmi.Gnmi.GetRequest;
 import gnmi.Gnmi.Path;
+//import gnmi.Gnmi.SubscribeRequest;
 import org.onosproject.gnmi.api.GnmiClient;
 import org.onosproject.gnmi.api.GnmiController;
 import org.onosproject.gnmi.api.GnmiEvent;
@@ -31,6 +38,9 @@ import org.onosproject.net.DeviceId;
 import org.onosproject.gnmi.api.GnmiEventListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Skeletal ONOS application component.
@@ -40,6 +50,9 @@ public final class Testgnmi {
 
     @Reference(cardinality = ReferenceCardinality.MANDATORY)
     protected GnmiController gnmiController;
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY)
+    protected DeviceService deviceService;
 
     private final Logger log = LoggerFactory.getLogger(getClass());
 
@@ -59,23 +72,76 @@ public final class Testgnmi {
             .addElem(Gnmi.PathElem.newBuilder().setName("state").build())
             .build();
 
-
     @Activate
     public void activate() {
         log.info("############## ATTIVATO ###########" + gnmiController.toString());
         gnmiController.addListener(deviceListener);
         gnmiClient = gnmiController.get(deviceId);
         log.info("############## ATTIVATO ###########" + gnmiClient.toString());
-        log.info("GET PATH" + path.toString());
+        //log.info("GET PATH" + path.toString());
 
+        // indagare meglio
         GetRequest request = GetRequest.newBuilder()
                 .addPath(path)
-                //.setType(GetRequest.DataType.ALL)
+                .setType(GetRequest.DataType.ALL)
                 .setEncoding(Gnmi.Encoding.PROTO)
                 .build();
 
-        log.info("NEW PATH" + Futures.getUnchecked(gnmiClient.get(request)).toString());
+        //log.info("NEW PATH" + Futures.getUnchecked(gnmiClient.get(request)).toString());
 
+        //log.info("CAPABILITIES" + Futures.getUnchecked(gnmiClient.capabilities()).toString());
+
+        log.info("CAPABILITIES" + Futures.getUnchecked(gnmiClient.capabilities()).toString());
+        log.info("CAPABILITIES default instance " + Futures.getUnchecked(gnmiClient.capabilities())
+                .getDefaultInstanceForType().toString());
+        log.info("CAPABILITIES model list" + Futures.getUnchecked(gnmiClient.capabilities())
+                .getSupportedModelsList().toString());
+        log.info("CAPABILITIES extentsion list" + Futures.getUnchecked(gnmiClient.capabilities())
+                .getExtensionList().toString());
+        log.info("CAPABILITIES gnmi version" + Futures.getUnchecked(gnmiClient.capabilities())
+                .getGNMIVersion());
+
+
+        Path patht = Path.newBuilder()
+                .addElem(Gnmi.PathElem.newBuilder().setName("interfaces").build())
+                .addElem(Gnmi.PathElem.newBuilder().setName("interface").putKey("name", "s1-eth1").build())
+                .build();
+    /*
+        GetRequest requestt = GetRequest.newBuilder()
+                .addPath(patht)
+                .setType(GetRequest.DataType.valueOf( "s1-eth1"))
+                .setEncoding(Gnmi.Encoding.PROTO)
+                .build();
+*/
+        //log.info(" PATH 2" + Futures.getUnchecked(gnmiClient.get(requestt)).toString());
+
+        /**
+         * Subscription code non serve perchè c'è già di partenza
+         */
+
+        Set<PortNumber> ports = deviceService.getPorts(deviceId).stream()
+                .map(Port::number)
+                .collect(Collectors.toSet());
+
+        log.info("porte" + ports.toString() + ports.iterator().next().name());
+        SubscriptionList subscriptionList = SubscriptionList.newBuilder()
+                .setMode(SubscriptionList.Mode.POLL)
+                //.setUpdatesOnly(true)
+                /*.addAllSubscription(ports.stream().map(
+                        port -> Subscription.newBuilder()
+                                .setPath(interfaceOperStatusPath(port.name()))
+                                .setMode(SubscriptionMode.SAMPLE)
+                                .build()).collect(Collectors.toList()))*/
+                .addSubscription(Subscription.newBuilder().setPath(interfaceOperStatusPath("s1-eth1"))
+                .setMode(SubscriptionMode.SAMPLE))
+                .build();
+
+        log.info("sub" + subscriptionList.toString());
+
+        /*gnmiController.get(deviceId).subscribe(
+                SubscribeRequest.newBuilder()
+                        .setSubscribe(subscriptionList)
+                        .build());*/
 
     }
 
@@ -84,12 +150,14 @@ public final class Testgnmi {
 
     }
 
-    private void checkSubscription(DeviceId deviceId) {
-        if (gnmiController.get(deviceId) == null) {
-            // Ignore devices for which a gNMI client does not exist.
-            return;
-        }
-
+    private Path interfaceOperStatusPath(String interfaceName) {
+        return Path.newBuilder()
+                .addElem(Gnmi.PathElem.newBuilder().setName("interfaces").build())
+                .addElem(Gnmi.PathElem.newBuilder()
+                        .setName("interface").putKey("name", "s1-eth1").build())
+                //.addElem(Gnmi.PathElem.newBuilder().setName("state").build())
+                //.addElem(Gnmi.PathElem.newBuilder().setName("oper-status").build())
+                .build();
     }
 
     private class InnerDeviceListener implements GnmiEventListener {
